088b8e0642c6cc218f5cd9980f455eeec70d5d9d
Tomer Ben Ari <tomerba6@outlook.com>
2020-10-12 16:49:41 +0300

    Test message

---
+++ test.java
@@ -0,0 +1,14 @@
+/**
+ * test
+ */
+import java.util.Scanner;
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        Scanner input = new Scanner(System.in);
+        String x = input.nextLine();
+        input.close();
+        System.out.println(x);
+    }
+}
