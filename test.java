91ad3ccc79b364949867f9982bb220053bbbbd79
Tomer Ben Ari <tomerba6@outlook.com>
2020-10-12 16:57:38 +0300

    Test stack

--- test.java
+++ test.java
@@ -2,6 +2,7 @@
  * test
  */
 import java.util.Scanner;
+import java.util.Stack;
 public class test {
 
     public static void main(String[] args) {
@@ -10,5 +11,8 @@ public class test {
         String x = input.nextLine();
         input.close();
         System.out.println(x);
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(20);
+        System.out.println(stack.pop());
     }
 }
